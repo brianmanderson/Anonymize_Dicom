47f756d0da07c16f9f9e3071cb84ce393df19a6c
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-12-02 13:42:23 -0800

    break down sections, make it more modular

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-from src.AnonymizeDicom import anonymize_dicom_down_path
+import pydicom
 
-path = r'C:\Users\b5anderson\Desktop\Files\Input'
-output = r'C:\Users\b5anderson\Desktop\Files\Output'
-anonymize_dicom_down_path(input_path=path, output_path=output)
+from src.AnonymizeDicom.Anonymize_Dicom import anonymize_list_of_files, anonymize_datastructure
+from DicomRTTool.ReaderWriter import DicomReaderWriter
+import os
+from glob import glob
+
+path = r'O:\DICOM\Test2\Input'
+output = r'O:\DICOM\Test2\Output'
+reader = DicomReaderWriter()
+reader.down_folder(input_path=path)
+patient_index = 0
+patient_ids_index = {}
+for index in reader.series_instances_dictionary:
+    pat_id = reader.series_instances_dictionary[index]['PatientID']
+    if pat_id not in patient_ids_index:
+        patient_ids_index[pat_id] = patient_index
+        patient_index += 1
+all_files = glob(os.path.join(path, '*.dcm'))
+anonymized_files = []
+for index in reader.series_instances_dictionary:
+    files = reader.return_files_from_index(index)
+    anonymized_files += files
+    iteration = patient_ids_index[reader.series_instances_dictionary[index]['PatientID']]
+    out_path = os.path.join(output, 'Anonymized_Patient_{}'.format(iteration))
+    anonymize_list_of_files(files=files, out_path=out_path, anon_index=iteration)
+for file in all_files:
+    if file not in anonymized_files:
+        ds = pydicom.read_file(file)
+        pat_id = ds.PatientID
+        if pat_id not in patient_ids_index:
+            patient_ids_index[pat_id] = patient_index
+            patient_index += 1
+        anon_index = patient_ids_index[pat_id]
+        output_path = os.path.join(output, 'Anonymized_Patient_{}'.format(anon_index))
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        anonymize_datastructure(ds=ds, out_path=output_path, anon_index=anon_index)
--- src/AnonymizeDicom/Anonymize_Dicom.py
+++ src/AnonymizeDicom/Anonymize_Dicom.py
@@ -7,29 +7,31 @@ from DicomRTTool.ReaderWriter import DicomReaderWriter
 import typing
 
 
-def anonymize_list_of_files(files, out_path_base, anon_index):
-    out_path = os.path.join(out_path_base, 'Anonymized_Patient_{}'.format(anon_index))
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
+def anonymize_datastructure(ds, out_path, anon_index):
     PatientID = "Patient_{}".format(anon_index)
     today = datetime.datetime.today()
-    AcquistionDate = ContentDate = StudyDate = PatientsBirthDate = "{}{}{}".format(today.year, today.month, today.day)
+    PatientsBirthDate = "{}{}{}".format(today.year, today.month, today.day)
     PatientName = "Anonymized_{}".format(anon_index)
-    modality_names = {}
+    modality = ds.Modality
+    ds.__setattr__("PatientID", PatientID)
+    ds.__setattr__("OtherPatientIDs", PatientID)
+    ds.__setattr__("PatientName", PatientName)
+    ds.__setattr__("PatientBirthDate", PatientsBirthDate)
+    for key in ["PhysiciansOfRecord", "ReferringPhysicianName", "StationName", "StudyDescription", "OperatorsName",
+                "InstitutionName", "InstitutionAddress", "ReviewerName"]:
+        if key in ds:
+            ds.__setattr__(key, "Null")
+    pydicom.write_file(os.path.join(out_path, "{}_{}.dcm".format(ds.Modality, ds.SOPInstanceUID)), ds)
+    return None
+
+
+def anonymize_list_of_files(files, out_path, anon_index):
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
     for file in files:
         ds = pydicom.read_file(file)
-        modality = ds.Modality
-        if modality not in modality_names:
-            modality_names[modality] = 1
-        ds.PatientID = PatientID
-        ds.OtherPatientIDs = PatientID
-        ds.AcquistionDate = AcquistionDate
-        ds.ContentDate = ContentDate
-        ds.StudyDate = StudyDate
-        ds.PatientsBirthDate = PatientsBirthDate
-        ds.PatientName = PatientName
-        pydicom.write_file(os.path.join(out_path, "{}_{}.dcm".format(modality, modality_names[modality])), ds)
-        modality_names[modality] += 1
+        anonymize_datastructure(ds=ds, out_path=out_path, anon_index=anon_index)
+    return None
 
 
 class DicomAnonymizerClass(object):
@@ -81,9 +83,17 @@ def anonymize_dicom_down_path(input_path: typing.Union[str, bytes, os.PathLike],
     For each patient, we will need to anonymize the images, structures, plans, and dose files while ensuring
     we still maintain a consistent identifier
     """
+    patient_index = 0
+    patient_ids_index = {}
+    for index in reader.series_instances_dictionary:
+        pat_id = reader.series_instances_dictionary[index]['PatientID']
+        if pat_id not in patient_ids_index:
+            patient_ids_index[pat_id] = patient_index
+            patient_index += 1
     for index in reader.series_instances_dictionary:
         files = reader.return_files_from_index(index)
-        anonymizer.change_dicom_image_files(files=files, out_path_base=output_path, iteration=index)
+        iteration = patient_ids_index[reader.series_instances_dictionary[index]['PatientID']]
+        anonymizer.change_dicom_image_files(files=files, out_path_base=output_path, iteration=iteration)
     xxx = 1
 
 
